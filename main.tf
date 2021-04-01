422adb8be18b7de5423215b22067b6a624d8c722
jpclipffel <jean-philippe.clipffel@post.lu>
2021-04-01 06:55:45 +0000

    Updated to production

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 module "masters-kirchberg" {
   # Meta
   source               = "./modules/node"
-  instances            = 1
+  instances            = 2
   name_seek            = 1
   name_template        = var.master_name_template
   # General
@@ -28,7 +28,7 @@ module "masters-kirchberg" {
 module "masters-gare" {
   # Meta
   source               = "./modules/node"
-  instances            = 0
+  instances            = 1
   name_seek            = 3
   name_template        = var.master_name_template
   # General
@@ -49,16 +49,39 @@ module "masters-gare" {
   disk_system_size     = 64
 }
 
-
 module "worker-kirchberg" {
   # Meta
   source               = "./modules/node"
-  instances            = 2
+  instances            = 3
   name_seek            = 1
   name_template        = var.worker_name_template
   # General
   resource_pool_id     = data.vsphere_resource_pool.kirchberg_low.id
   datastore_id         = data.vsphere_datastore.kirchberg_02.id
+  vm_folder            = "_root/DATI : ICT/K8S/k8s-ict-prod"
+  num_cpus             = 8
+  memory               = 8192
+  guest_id             = data.vsphere_virtual_machine.template.guest_id
+  template_uuid        = data.vsphere_virtual_machine.template.id
+  # Network
+  network_server_id    = data.vsphere_network.k8s_servers.id
+  network_server_ips   = var.network_server_ips
+  network_client_id    = data.vsphere_network.k8s_servers.id
+  network_client_ips   = var.network_client_ips
+  # Disk
+  disk_system_label    = "disk0"
+  disk_system_size     = 64
+}
+
+module "worker-gare" {
+  # Meta
+  source               = "./modules/node"
+  instances            = 3
+  name_seek            = 4
+  name_template        = var.worker_name_template
+  # General
+  resource_pool_id     = data.vsphere_resource_pool.gare_low.id
+  datastore_id         = data.vsphere_datastore.gare_02.id
   vm_folder            = "_root/DATI : ICT/K8S/k8s-ict-prod"
   num_cpus             = 8
   memory               = 8192
--- variables.tf
+++ variables.tf
@@ -42,17 +42,20 @@ variable "network_server_ips" {
   default = {
     # Masters
     "k8s-ict-master-01" = "172.29.42.21/24",
-    # "k8s-ict-master-02" = "172.29.42.22/24",
-    # "k8s-ict-master-03" = "172.29.42.23/24",
+    "k8s-ict-master-02" = "172.29.42.22/24",
+    "k8s-ict-master-03" = "172.29.42.23/24",
     # Workers
-    "k8s-ict-worker-01" = "172.29.42.22/24",
-    "k8s-ict-worker-02" = "172.29.42.23/24",
-    # "k8s-ict-worker-03" = "172.29.42.33/24",
-    # "k8s-ict-worker-04" = "172.29.42.34/24",
-    # "k8s-ict-worker-05" = "172.29.42.35/24",
-    # "k8s-ict-worker-06" = "172.29.42.36/24",
+    "k8s-ict-worker-01" = "172.29.42.31/24",
+    "k8s-ict-worker-02" = "172.29.42.32/24",
+    "k8s-ict-worker-03" = "172.29.42.33/24",
+    "k8s-ict-worker-04" = "172.29.42.34/24",
+    "k8s-ict-worker-05" = "172.29.42.35/24",
+    "k8s-ict-worker-06" = "172.29.42.36/24",
+    # Future scaling
     # "k8s-ict-worker-07" = "172.29.42.37/24",
     # "k8s-ict-worker-08" = "172.29.42.38/24",
+    # "k8s-ict-worker-09" = "172.29.42.39/24",
+    # "k8s-ict-worker-10" = "172.29.42.40/24",
   }
 }
 
@@ -61,16 +64,19 @@ variable "network_client_ips" {
   default = {
     # Masters
     "k8s-ict-master-01" = "172.29.43.21/24",
-    # "k8s-ict-master-02" = "172.29.43.22/24",
-    # "k8s-ict-master-03" = "172.29.43.23/24",
+    "k8s-ict-master-02" = "172.29.43.22/24",
+    "k8s-ict-master-03" = "172.29.43.23/24",
     # Workers
-    "k8s-ict-worker-01" = "172.29.43.22/24",
-    "k8s-ict-worker-02" = "172.29.43.23/24",
-    # "k8s-ict-worker-03" = "172.29.43.33/24",
-    # "k8s-ict-worker-04" = "172.29.43.34/24",
-    # "k8s-ict-worker-05" = "172.29.43.35/24",
-    # "k8s-ict-worker-06" = "172.29.43.36/24",
+    "k8s-ict-worker-01" = "172.29.43.31/24",
+    "k8s-ict-worker-02" = "172.29.43.32/24",
+    "k8s-ict-worker-03" = "172.29.43.33/24",
+    "k8s-ict-worker-04" = "172.29.43.34/24",
+    "k8s-ict-worker-05" = "172.29.43.35/24",
+    "k8s-ict-worker-06" = "172.29.43.36/24",
+    # Future scaling
     # "k8s-ict-worker-07" = "172.29.43.37/24",
     # "k8s-ict-worker-08" = "172.29.43.38/24",
+    # "k8s-ict-worker-09" = "172.29.43.39/24",
+    # "k8s-ict-worker-10" = "172.29.43.40/24",
   }
 }
